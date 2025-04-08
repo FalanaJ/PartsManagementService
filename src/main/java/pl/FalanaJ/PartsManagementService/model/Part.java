33edc4ee8f1869b19a5ccc2289b0183317863653
FalanaJ <jakubfalana5@gmail.com>
2025-04-08 11:10:48 +0200

    Add PostreSQL connection config and try add OracleDb config

--- src/main/java/pl/FalanaJ/PartsManagementService/model/Part.java
+++ src/main/java/pl/FalanaJ/PartsManagementService/model/Part.java
@@ -2,11 +2,13 @@ package pl.FalanaJ.PartsManagementService.model;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @EqualsAndHashCode
 @Data
 @Entity
+@Table(name = "parts")
 public class Part {
 
     @EmbeddedId
